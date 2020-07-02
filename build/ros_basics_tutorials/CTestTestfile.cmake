fa4b0969e9889de4481c56b66210bb78405a68e8
curtis <curtis@rosmaster.fios-router.home>
2020-07-02 02:39:26 -0400

    removing build

--- build/ros_basics_tutorials/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/curtis/dev/ros-helloworld/src/ros_basics_tutorials
-# Build directory: /home/curtis/dev/ros-helloworld/build/ros_basics_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
